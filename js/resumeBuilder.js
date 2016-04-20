48b6871feea5652dfc0ac4e54bbddd6ae6e8ee28
troy-lamerton <troy.lamerton@gmail.com>
2016-04-20 21:00:45 +1200

    Remove some html comments and add header content created by javascript

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,42 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {name: "Troy Lamerton",
+           role: "Junior Web Developer",
+           email: "troy.lamerton@gmail.com",
+           picture: "http://www.gravatar.com/avatar/0eb38fc6ee95b244b8d45757ee242763",
+           welcome: "Kia ora! This is my professional resume.",
+           skills: ["collaborating", "building websites", "analysis"]
+         };
+var allSkills = HTMLskillsStart;
+
+// add list item for each skill in array
+for (var i in bio["skills"]) {
+  allSkills += HTMLskills.replace('%data%', bio["skills"][i]);
+};
+/* End of bio */
+
+var work = {
+  position: "Content Loader",
+  employer: "Education Review Office",
+  years: 1,
+  city: "Wellington"
+};
+
+var education = {
+  name: "Victoria University",
+  years: 1.5,
+  city: "Wellington"
+};
+
+
+var formattedName = HTMLheaderName.replace('%data%', bio.name);
+var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+var myPicture = HTMLbioPic.replace('%data%', bio.picture);
+var myEmail = HTMLemail.replace('%data%', bio.email);
+var myWelcomeMessage = HTMLwelcomeMsg.replace('%data%', bio.welcome);
+
+$('#header').prepend(formattedName);
+$('#header').append(formattedRole);
+$('#header').append(myWelcomeMessage);
+
+$('#topContacts').append(myEmail);
+$('#header').append(myPicture);
+$('#header').append(allSkills);
